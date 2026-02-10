09f0e9ae2c2f2b2782392371f0ae3d15073b69d4
Daniel <daniel.jach@outlook.com>
2026-02-10 16:32:47 +0100

    upload recent changes

--- scripts/scrape_beratungsprotokolle-fdm.R
+++ scripts/scrape_beratungsprotokolle-fdm.R
@@ -67,44 +67,10 @@ saveRDS(df, "data/data_processed/beratungsprotokolle-fdm.Rda")
 
 
 
+df <- readRDS("data/data_processed/beratungsprotokolle-fdm.Rda") |> tibble()
 
-df <- df |> 
-  filter(complete.cases(df)) |> 
-  mutate(dates = as.numeric(dates)) |> 
-  filter(dates < 2026) |> 
-  mutate(
-    faks = as.numeric(faks), 
-    fakType = case_when(
-      faks %in% c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) ~ "Lower Faculties",
-      faks %in% c(11) ~ "Business and Economics",
-      faks %in% c(12) ~ "Education & Psychology",
-      faks %in% c(13) ~ "Rehabilitation",
-      faks %in% c(14) ~ "Humanities and Theology",
-      faks %in% c(15) ~ "Cultural Studies",
-      faks %in% c(16) ~ "Arts and Sports",
-      faks %in% c(17) ~ "Social Sciences",
-      TRUE ~ as.character(faks)  
-    )
-  ) |> 
-  mutate(fakType = reorder(fakType, faks)) 
-  
 
 df |> 
-  count(fakType, dates) |>
-  group_by(fakType) |> 
-  mutate(cumSum = cumsum(n)) |> 
-  ggplot(aes(x = dates, y = cumSum, group = fakType)) +
-  geom_point() +
-  geom_line() +
-  facet_wrap(~fakType) +
-  theme_minimal()
+  filter(names == "Watzlawik")
 
-df |> 
-  mutate(faks = as.character(faks)) |> 
-  count(faks, dates) |>
-  group_by(dates) |> 
-  ggplot(aes(x = dates, y = n, group = reorder(faks, -as.numeric(faks)), fill = reorder(faks, -as.numeric(faks)))) +
-  geom_bar(stat = 'identity', color = "black") +
-  scale_fill_viridis_d(direction = -1) +
-  theme_minimal() +
-  labs(fill = "Fakultäten", x = "Jahr")
+
